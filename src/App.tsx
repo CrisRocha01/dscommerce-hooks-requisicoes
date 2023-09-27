02318681e988922c4aa52fb06259b6930cde565e
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2023-09-26 22:18:55 -0300

    First back end requests

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Catalog from "./routes/ClientHome/Catalog";
+import ProductDetails from "./routes/ClientHome/ProductDetails";
+import ClientHome from "./routes/ClientHome";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ClientHome />}>
+                    <Route index element={<Catalog />} />
+                    <Route path="catalog" element={<Catalog />} />
+                    <Route path="product-details/:productId" element={<ProductDetails />} />
+                </Route>
+                <Route path="*"element={<Navigate to='/'/>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,35 @@
+import "./ProductDetails.css";
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import * as productService from "../../../services/product-service";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+
+export default function ProductDetails() {
+    const params = useParams();
+
+    const [product, setProduct] = useState<ProductDTO>();
+
+    useEffect(() => {
+        productService.findById(Number(params.productId)).then((response) => {
+            setProduct(response.data);
+        });
+    }, []);
+
+    return (
+        <main>
+            <section id="product-details-section" className="dsc-container">
+                {product && <ProductDetailsCard product={product} />}
+                <div className="dsc-btn-page-container">
+                    <ButtonPrimary text="Comprar" />
+                    <Link to={"/"}>
+                        <ButtonInverse text="Início" />
+                    </Link>
+                </div>
+            </section>
+        </main>
+    );
+}
